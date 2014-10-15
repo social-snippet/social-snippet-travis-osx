315708d1dcbfc5009de83e17097abae9827e97aa
Hiroyuki Sano <sh19910711@gmail.com>
2014-10-15 12:33:32 +0900

    wip: command_line: sspm: add options sspm-search

--- lib/social_snippet/command_line/sspm/sub_commands/search_command.rb
+++ lib/social_snippet/command_line/sspm/sub_commands/search_command.rb
@@ -14,6 +14,7 @@ module SocialSnippet
           def initialize(new_args)
             super
 
+            @flag_has_options = false
             @query = args.find {|arg| is_not_line_option? arg }
 
             @client = ::SocialSnippet::RegistryClient.new(
@@ -24,16 +25,76 @@ module SocialSnippet
           end
 
           def define_options
+            # show name
+            opt_parser.on "-n", "--[no-]name" do |v|
+              options[:show_name] = v
+              @flag_has_options = true
+            end
+
+            # show desc
+            opt_parser.on "-d", "--[no-]desc" do |v|
+              options[:show_desc] = v
+              @flag_has_options = true
+            end
+
+            # show url
+            opt_parser.on "-u", "--[no-]url" do |v|
+              options[:show_url] = v
+              @flag_has_options = true
+            end
           end
 
           def set_default_options
+            unless @flag_has_options
+              options[:show_name] = true if options[:show_name].nil?
+              options[:show_desc] = true if options[:show_desc].nil?
+              options[:show_url]  = false if options[:show_url].nil?
+            end
+
+            options[:show_name] = false if options[:show_name].nil?
+            options[:show_desc] = false if options[:show_desc].nil?
+            options[:show_url]  = false if options[:show_url].nil?
           end
 
           def run
-            # TODO: change host
             client.get_repositories(query).each do |repo|
-              puts "%s: %s" % [repo["name"], repo["desc"]]
+              puts output_format % output_list(repo)
             end
+          end
+
+          private
+
+          def output_format
+            f = ""
+            if options[:show_name]
+              f += "%s"
+            end
+            if options[:show_desc]
+              if options[:show_name]
+                f += ": "
+              end
+              f += "%s"
+            end
+            if options[:show_url]
+              if options[:show_name] && options[:show_desc]
+                f += " (%s)"
+              elsif options[:show_name]
+                f += ": %s"
+              elsif options[:show_desc]
+                f += " (%s)"
+              else
+                f += "%s"
+              end
+            end
+            return f
+          end
+
+          def output_list(repo)
+            list = []
+            list.push repo["name"] if options[:show_name]
+            list.push repo["desc"] if options[:show_desc]
+            list.push repo["url"] if options[:show_url]
+            return list
           end
 
         end
